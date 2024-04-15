cb3be8d6cb0e416b391d6854f66eaca7cb77add9
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-16 03:30:13 +0800

    feat: Add rendered empty DayRecordsScreen

---
+++ homewarden/screens/DayRecordsScreen.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+
+import Section from '../components/Section';
+
+export default function DayRecordsScreen() {
+    return (
+        <>
+            <Section title="Day Records Screen">
+                <Text>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ducimus eligendi quod ex, saepe facilis mollitia qui facere possimus.
+                </Text>
+            </Section>
+        </>
+    );
+}
