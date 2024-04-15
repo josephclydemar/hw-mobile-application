970e300a29d946377342df7674f40065f556a47c
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-16 03:17:54 +0800

    refactor: Use the useState hook instead of the useReducer hook for storing the value

--- homewarden/contexts/AuthorizedUsersContext.tsx
+++ homewarden/contexts/AuthorizedUsersContext.tsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { createContext, Context, ReactNode, useState, useEffect } from 'react';
+import { createContext, Context, ReactNode, useState } from 'react';
 
 import { AuthorizedUser, AuthorizedUserContextType } from '../types/UsersTypes';
 
@@ -7,27 +6,10 @@ type AuthorizedUserContextProviderProps = {
     children: ReactNode;
 };
 
-type SuwayRa = {
-    _id: string;
-    name: string;
-    createdAt: string;
-    updatedAt: string;
-    __v: number;
-};
-
 const AuthorizedUsersContext: Context<AuthorizedUserContextType | null> = createContext<AuthorizedUserContextType | null>(null);
 
 export function AuthorizedUsersContextProvider({ children }: AuthorizedUserContextProviderProps) {
     const [authorizedUsers, setAuthorizedUsers] = useState<AuthorizedUser[]>([]);
-    useEffect(function (): void {
-        axios.get('http://192.168.1.2:8500/api/v1/authorized_users').then(function ({ data }): void {
-            setAuthorizedUsers(() =>
-                data.map(function (item: SuwayRa): AuthorizedUser {
-                    return { id: item._id, name: item.name, createdAt: item.createdAt };
-                }),
-            );
-        });
-    }, []);
     return <AuthorizedUsersContext.Provider value={{ authorizedUsers, setAuthorizedUsers }}>{children}</AuthorizedUsersContext.Provider>;
 }
 
