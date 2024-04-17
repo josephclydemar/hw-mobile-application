154399a242166c89b29c6df728d8b1a1750e891f
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-17 16:28:31 +0800

    feat(software/mobile-application): Add middle JSX component for easier screen navigation

---
+++ homewarden/Root.tsx
@@ -0,0 +1,64 @@
+import { Context, useEffect, useContext, useState } from 'react';
+import { View } from 'react-native';
+
+// contexts
+import { CurrentDayDetectionsContextProvider } from './contexts/CurrentDayDetectionsContext';
+import { DayRecordsContextProvider } from './contexts/DayRecordsContext';
+import { AuthorizedUsersContextProvider } from './contexts/AuthorizedUsersContext';
+
+import CurrentScreenContext from './contexts/CurrentScreenContext';
+
+// components
+import NavigationBar from './components/NavigationBar';
+import ContextsWrapper from './components/ContextsWrapper';
+
+// screens
+import HomeScreen from './screens/HomeScreen';
+import DayRecordsScreen from './screens/DayRecordsScreen';
+import AuthorizedUsersScreen from './screens/AuthorizedUsersScreen';
+import SelectedAuthorizedUserScreen from './screens/SelectedAuthorizedUserScreen';
+import SelectedDayRecordsScreen from './screens/SelectedDayRecordsScreen';
+
+// types
+import { CurrentScreenContextType } from './types/ScreensTypes';
+
+export default function Root() {
+    const { currentScreen, setCurrentScreen } = useContext(CurrentScreenContext as Context<CurrentScreenContextType>);
+    const [currentScreenComponent, setCurrentScreenComponent] = useState<JSX.Element>(<HomeScreen />);
+    useEffect(
+        function () {
+            switch (currentScreen) {
+                case 'home-screen':
+                    setCurrentScreenComponent(<HomeScreen />);
+                    break;
+                case 'day-records-screen':
+                    setCurrentScreenComponent(<DayRecordsScreen />);
+                    break;
+                case 'authorized-users-screen':
+                    setCurrentScreenComponent(<AuthorizedUsersScreen />);
+                    break;
+                case 'selected-authorized-user-screen':
+                    setCurrentScreenComponent(<SelectedAuthorizedUserScreen />);
+                    break;
+                case 'selected-day-records-screen':
+                    setCurrentScreenComponent(<SelectedDayRecordsScreen />);
+                    break;
+            }
+        },
+        [currentScreen],
+    );
+    return (
+        <>
+            <CurrentDayDetectionsContextProvider>
+                <DayRecordsContextProvider>
+                    <AuthorizedUsersContextProvider>
+                        <ContextsWrapper>{currentScreenComponent}</ContextsWrapper>
+                    </AuthorizedUsersContextProvider>
+                </DayRecordsContextProvider>
+            </CurrentDayDetectionsContextProvider>
+            <View>
+                <NavigationBar setCurrentScreen={setCurrentScreen} />
+            </View>
+        </>
+    );
+}
