80f619e3f67eae78e074b2ea68c1573cb54031a4
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-18 20:08:42 +0800

    feat: Add text input for new authorized user name

--- homewarden/screens/AddNewAuthorizedUserScreen.tsx
+++ homewarden/screens/AddNewAuthorizedUserScreen.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 export default function AddNewAuthorizedUserScreen() {
+    const [newAuthorizedUserName, setNewAuthorizedUserName] = useState<string>('');
     return (
         <View>
-            <Text></Text>
+            <Text>Add New Authorized User</Text>
+            <View>
+                <TextInput
+                    placeholder="New Authorized User Name..."
+                    onChangeText={function (textData: string): void {
+                        setNewAuthorizedUserName(() => textData);
+                    }}
+                    style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        borderColor: '#000',
+                    }}
+                />
+            </View>
         </View>
     );
 }
